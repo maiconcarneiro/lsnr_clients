2c7a03dc16f8e69ed0540ba62101282c39e64b4e
Maicon Carneiro <50035923+maiconcarneiro@users.noreply.github.com>
2022-01-12 19:48:50 -0300

    parametros

--- lsnr_clients.sh
+++ lsnr_clients.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
-# Blog do Dibiei | Aprender e compartilhar !
+# Blog do Dibiei | Aprender e compartilhar!
 # Maicon Carneiro | Salvador-BA, 08/01/2021
-# lsnr_clients.sh - v1.2
+# lsnr_clients.sh - v1.3
 # Script para analizar Log do Listener e gerar lista de IPs que conectaram no banco de dados Oracle
 #
 # Exemplo: ./lsnr_clients.sh 15 s PRD.dibiei.com LISTENER_PRD
 # Instrucoes de uso em dibiei.wordpress.com
+# wget "https://raw.githubusercontent.com/maiconcarneiro/lsnr_clients/main/lsnr_clients.sh"; chmod +x lsnr_clients.sh
 #
 # Data       | Autor              | Modificacao
 # 10/01/2022 | Maicon Carneiro    | Codigo para identificar o diretorio do listener automaticamente
+# 12/01/2022 | Maicon Carneiro    | Adicionado suporte a versao 11gR2 com ajuste na chamada do lsnrctl
+
+periodo="1"
+geraContagem="n"
+nomeServico=""
+nomeListener=""
+
+while getopts ":i:c:s:l:" opt; do
+  case $opt in
+    i) periodo="$OPTARG"
+    ;;
+    c) geraContagem="s"
+    ;;
+    s) nomeServico="$OPTARG"
+    ;;
+    l) nomeListener="$OPTARG"
+    ;;
+    \?) echo "Parametro invalido -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+done
 
 dataArquivo=$(date +'%H%M%S')
 dirInicial=$(pwd)
-periodo=$1
-geraContagem=$2
-nomeServico=$3
-nomeListener=$4
 
 arquivoConexoes="$dirInicial/lsnrchkip_conn_$dataArquivo.txt"
 arquivoListaIP="$dirInicial/lsnrchkip_list_$dataArquivo.txt"
 arquivoContagem="$dirInicial/lsnrchkip_cont_$dataArquivo.txt"
 arquivoContagemStage="$dirInicial/lsnrchkip_cont_stage_$dataArquivo.txt"
+arquivoScriptLsnrctl="$dirInicial/lsnrchkip_lsnrctl_$dataArquivo.sh"
+
 ORACLE_HOME=""
 
 LimpaArquivosTemporarios()
@@ -29,6 +51,7 @@ LimpaArquivosTemporarios()
    rm -f $arquivoListaIP
    rm -f $arquivoContagem
    rm -f $arquivoContagemStage
+   rm -f $arquivoScriptLsnrctl
 }
 
 GetOracleHome()
@@ -52,6 +75,11 @@ fi;
 linhaListener=$(ps -ef | grep tnslsnr | grep $nomeListener | egrep -v 'ASM|SCAN|MGM|grep' | head -n1 )
 ownerListener=$(echo "$linhaListener" | awk '{ print $1 }')
 
+if [ -z "$linhaListener" ]; then
+ echo "Erro: Listener $nomeListener deve estar online."
+ exit 1
+fi
+
 usuarioAtual=$(whoami)
 if [ "$usuarioAtual" != "$ownerListener" ]; then
 echo "Erro: O script deve ser executado com o usuario dono do Listener!"
@@ -61,7 +89,16 @@ fi
 
 binarioListener=$(echo "$linhaListener" | awk '{ print $8 }')
 GetOracleHome $binarioListener
-dirLogListener=$($ORACLE_HOME/bin/lsnrctl show log_directory $nomeListener | grep log_directory | awk '{ print $6 }' | sed -e "s/alert/trace/g" )
+
+# obtem o diretorio de log do listener (11gR2da problema quando nao usa o current_listener )
+echo "$ORACLE_HOME/bin/lsnrctl <<EOF
+set current_listener $nomeListener
+show log_directory
+EOF" > $arquivoScriptLsnrctl
+
+chmod +x $arquivoScriptLsnrctl
+dirLogListener=$(sh $arquivoScriptLsnrctl | grep log_directory | awk '{ print $6 }' | sed -e "s/alert/trace/g" )
+rm -f $arquivoScriptLsnrctl
 
 echo "*********************************************************************************"
 echo "Servidor..................: $(hostname)"
@@ -74,13 +111,13 @@ echo "Service Name..............: $nomeServico"
 echo ""
 echo "Arquivos de log analisados:"
 #echo "---------------------------------------------------------------------------------"
- find $dirLogListener -mtime -$periodo | grep .log | egrep -v '.gz|.zip'
+ find $dirLogListener -mtime -$periodo | grep -i "$nomeListener" | grep .log | egrep -v '.gz|.zip'
 echo ""
 echo "*********************************************************************************"
 
 LimpaArquivosTemporarios
 cd $dirLogListener
-for arquivo in $(find . -mtime -$periodo | grep .log | egrep -v '.gz|.zip')
+for arquivo in $(find . -mtime -$periodo | grep -i "$nomeListener" | grep .log | egrep -v '.gz|.zip')
 do
  grep -H CONNECT_DATA $arquivo | grep -i "$nomeServico" >> $arquivoConexoes
 done
